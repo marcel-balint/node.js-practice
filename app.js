fd980a481e13147c4547ad9bb550f7c7d93c6456
Marcel Balint <blank_blankness@yahoo.com>
2022-09-27 12:56:00 -0400

    Send a 404 page with Express, removed the server page

--- app.js
+++ app.js
@@ -13,3 +13,8 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
 });
+
+//404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
