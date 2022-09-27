25517a68c4e6ea64715ec49bb100cb43c81425d3
Marcel Balint <blank_blankness@yahoo.com>
2022-09-27 12:43:05 -0400

    Added Express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+//Express app
+const app = express();
+
+//Listen for requests
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
+  const num = _.random(0, 50);
+  console.log("Random :", num);
   console.log("Requested!!");
   res.setHeader("Content-type", "text/html");
 
